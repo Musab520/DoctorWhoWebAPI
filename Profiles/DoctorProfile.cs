6b138bd40f7350563d04833cbffa97885171f94e
Musab Abuasi <abuasimusab@gmail.com>
2022-09-07 12:48:59 +0300

    Create AutoMapper Profiles

---
+++ Profiles/DoctorProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using DoctorWho.Db;
+
+namespace DoctorWho{ 
+    public class DoctorProfile :Profile
+    {
+        public DoctorProfile()
+        {
+            CreateMap<Doctor, Web.DtoModels.DoctorDto>();
+            CreateMap<Web.DtoModels.DoctorForInsertDto,Doctor>();
+            CreateMap<Web.DtoModels.DoctorForUpdateDto,Doctor>();
+        }
+        
+    }
+}
+
