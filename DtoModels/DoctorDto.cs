b8f04acc48ce0ccf90a97f90bfe0f098ca99abc8
Musab Abuasi <abuasimusab@gmail.com>
2022-09-07 12:02:57 +0300

    Create DoctorDtos

---
+++ DtoModels/DoctorDto.cs
@@ -0,0 +1,16 @@
+﻿namespace DoctorWho.Web.DtoModels
+{
+    public class DoctorDto
+    {
+        public class Doctor
+        {
+            public int DoctorId { get; set; }
+            public int DoctorNumber { get; set; }
+            public string DoctorName { get; set; }
+            public DateTime BirthDate { get; set; }
+            public DateTime FirstEpisodeDate { get; set; }
+            public DateTime LastEpisodeDate { get; set; }
+           // public List<EpisodeDto> EpisodeList { get; set; }
+        }
+    }
+}
