08cbe98bd4404a300e4d197ba7fdc13611451373
Musab Abuasi <abuasimusab@gmail.com>
2022-09-11 01:30:29 +0300

    Added Author Repository

---
+++ Services/IAuthorRepository.cs
@@ -0,0 +1,11 @@
+﻿using DoctorWho.Db;
+
+namespace DoctorWho.Web.Services
+{
+    public interface IAuthorRepository
+    {
+        Task<bool> SaveChangesAsync();
+        Task<Author?> GetAuthorAsync(int AuthorId);
+
+    }
+}
