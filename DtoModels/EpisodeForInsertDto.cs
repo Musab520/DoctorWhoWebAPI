3273f6d7642c305d0a28fa0938be3e2b11bb25fb
Musab Abuasi <abuasimusab@gmail.com>
2022-09-10 11:29:35 +0300

    Create EpisodeDtos

---
+++ DtoModels/EpisodeForInsertDto.cs
@@ -0,0 +1,14 @@
+﻿namespace DoctorWho.Web.DtoModels
+{
+    public class EpisodeForInsertDto
+    {
+        public int? SeriesNumber { get; set; }
+        public int? EpisodeNumber { get; set; }
+        public string? EpisodeType { get; set; }
+        public string? Title { get; set; }
+        public DateTime? EpisodeDate { get; set; }
+        public int? AuthorId { get; set; }
+        public int? DoctorId { get; set; }
+        public string? Notes { get; set; }
+    }
+}
