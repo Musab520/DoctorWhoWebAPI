663c868a15c24968452d84a78749d0e34a27c89e
Musab Abuasi <abuasimusab@gmail.com>
2022-09-10 13:28:45 +0300

    Added Episode Repository

---
+++ Services/EpisodeRepository.cs
@@ -0,0 +1,30 @@
+﻿using DoctorWho.Db;
+using DoctorWho.Web.DtoModels;
+using Microsoft.EntityFrameworkCore;
+
+namespace DoctorWho.Web.Services
+{
+    public class EpisodeRepository : IEpisodeRepository
+    {
+        private readonly DoctorWhoCoreDbContext _context;
+
+        public EpisodeRepository(DoctorWhoCoreDbContext context)
+        {
+            _context = context ?? throw new ArgumentNullException();
+        }
+        public async Task<IEnumerable<Episode>> GetDoctorsAsync()
+        {
+            return await _context.episodes.ToListAsync();
+        }
+
+        public async Task InsertDoctorAsync(Episode episode)
+        {
+           await _context.episodes.AddAsync(episode);
+        }
+
+        public async Task<bool> SaveChangesAsync()
+        {
+            return (await _context.SaveChangesAsync() >= 0);
+        }
+    }
+}
---
+++ Services/IEpisodeRepository.cs
@@ -0,0 +1,12 @@
+﻿using DoctorWho.Db;
+using DoctorWho.Web.DtoModels;
+
+namespace DoctorWho.Web.Services
+{
+    public interface IEpisodeRepository
+    {
+        Task<IEnumerable<Episode>> GetDoctorsAsync();
+        Task InsertDoctorAsync(Episode episode);
+        Task<bool> SaveChangesAsync();
+    }
+}
