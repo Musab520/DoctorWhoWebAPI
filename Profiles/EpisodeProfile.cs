7c2c051be1eb5663b340e95c03b2d48701cdb64c
Musab Abuasi <abuasimusab@gmail.com>
2022-09-10 12:01:13 +0300

    Added Episode Mappings

---
+++ Profiles/EpisodeProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using DoctorWho.Db;
+using DoctorWho.Web.DtoModels;
+
+namespace DoctorWho.Web.Profiles
+{
+    public class EpisodeProfile : Profile
+    {
+        public EpisodeProfile()
+        {
+            CreateMap<Episode, EpisodeDto>();
+            CreateMap<EpisodeForInsertDto,Episode>();
+        }
+    }
+}
