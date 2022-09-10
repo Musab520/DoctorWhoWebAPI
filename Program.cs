812bace5f6e02d1e43036f2adf638b215608ad1a
Musab Abuasi <abuasimusab@gmail.com>
2022-09-11 02:29:28 +0300

    Added configuration connection string

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 using DoctorWho.Db;
 using DoctorWho.Web;
 using DoctorWho.Web.Services;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -10,7 +11,9 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddDbContext<DoctorWhoCoreDbContext>();
+builder.Services.AddDbContext<DoctorWhoCoreDbContext>(
+    dbContextOptions => dbContextOptions.UseSqlServer(
+        builder.Configuration["ConnectionStrings:DoctorWhoDb"])) ;
 builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 builder.Services.AddScoped<IDoctorRepository, DoctorRepository>();
 builder.Services.AddScoped<IEpisodeRepository, EpisodeRepository>();
